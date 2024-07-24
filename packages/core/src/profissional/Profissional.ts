c73284a05700fd136c5ec8950ca070b87d134244
Anderson Almeida <andersonpa10pa@gmail.com>
2024-07-24 15:52:49 -0300

    Criação das resgras de negocio inicial da aplicação.

---
+++ packages/core/src/profissional/Profissional.ts
@@ -0,0 +1,9 @@
+export default interface Profissional {
+  id: number
+  nome: string
+  descricao: string
+  imageURL: string
+  avaliacao: number
+  quantidadeAvaliacoes: number
+  // agendamentos ?: Agendamento[]
+}
